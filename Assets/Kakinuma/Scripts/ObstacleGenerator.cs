146b79c929fcef7bdb81172ef212acbb03734b6c
KakinumaYuzuki <V.proYuzu23@gmail.com>
2023-08-20 17:03:14 +0900

    [update]レベルデザイン

--- Assets/Kakinuma/Scripts/ObstacleGenerator.cs
+++ Assets/Kakinuma/Scripts/ObstacleGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 /// <summary>
 /// 隕石生成のためのスクリプト
@@ -12,16 +13,30 @@ public class ObstacleGenerator : MonoBehaviour
     [SerializeField] GameObject[] _obstaclePrefab;
     [Tooltip("生成する場所(複数可)")]
     [SerializeField] Transform[] _transform;
-    [Tooltip("シーン上にある動く隕石の数の制限")]
+    [Tooltip("シーン上にある動く隕石の数の制限(使用不可のためインターバルで操作)")]
     [SerializeField] int _obstacleLimit = 30;
+    [Tooltip("生成の時間間隔")]
+    [SerializeField] float _interval = 3f;
+    float _timer;
+    GameObject _obstacles;
+
+    void Start()
+    {
+        _obstacles = GameObject.FindWithTag("MeteoriteParent");
+    }
 
     void Update()
     {
-        if (_obstacleLimit > this.GetComponentsInChildren<MeteoriteMove>().Length) //シーン上の隕石が制限数より少なくなったら生成
+        if (_obstacleLimit > _obstacles.GetComponentsInChildren<MeteoriteMove>().Length) //シーン上の隕石が制限数より少なくなったら生成
         {
-            int indexObject = Random.Range(0, _obstaclePrefab.Length);
-            int indexPosition = Random.Range(0, _transform.Length);
-            Instantiate(_obstaclePrefab[indexObject], _transform[indexPosition].position, _obstaclePrefab[indexObject].transform.rotation);
+            _timer += Time.deltaTime;
+            if (_timer > _interval)
+            {
+                _timer = 0;
+                int indexObject = Random.Range(0, _obstaclePrefab.Length);
+                int indexPosition = Random.Range(0, _transform.Length);
+                Instantiate(_obstaclePrefab[indexObject], _transform[indexPosition].position, _obstaclePrefab[indexObject].transform.rotation);
+            }
         }
     }
 }
--- Assets/Kakinuma/Scripts/ReturnCheck.cs
+++ Assets/Kakinuma/Scripts/ReturnCheck.cs
@@ -13,6 +13,9 @@ public class ReturnCheck : MonoBehaviour
     [Tooltip("帰りの障害物(親オブジェクト)")]
     [SerializeField] GameObject _returnObstacle;
     [Tooltip("往復判定(チェックで帰りの判定)")]
+    [SerializeField] GameObject _enemyGenerator1;
+    [SerializeField] GameObject _enemyGenerator2;
+
     public bool _return = false; //往復の判定(行き)
     private void OnTriggerEnter(Collider other)
     {
@@ -21,6 +24,9 @@ public class ReturnCheck : MonoBehaviour
             _return = true; //往復の判定(帰り)
             Destroy(_obstacle);
             _returnObstacle.SetActive(true);
+            Destroy(_enemyGenerator1);
+            Destroy(_enemyGenerator2);
+            AudioManager.instance.PlaySE(5);
         }
     }
 }
